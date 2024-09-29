1a818800c5036ea6e9dfe574a284d3abd1a4bd99
Amir Khan <85296624+Devil56@users.noreply.github.com>
2024-09-29 08:26:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 4); 
+
+console.log("Amir")
+
+let name = "amir"
+let age = 25
+let isLoggedIn = false 
+
+// number => 2 to pwr 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Amir")
+console.log(typeof age)
+
+console.log(typeof null); // null is a object
+console.log(typeof undefined); // undefined
+
+
