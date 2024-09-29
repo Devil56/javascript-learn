7b05fbe651ca86f63a163dc11434108f1ccc222b
Amir Khan <85296624+Devil56@users.noreply.github.com>
2024-09-29 15:07:02 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+// let score = false
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan(not a number)
+// true => 1; false => 0
+
+
+// let isLoggedIn = "Amir"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Amir" => true
+// 56 => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
