99005050e70eed0ab37239ea1a4ca06909607e9b
Amir Khan <85296624+Devil56@users.noreply.github.com>
2024-09-30 16:21:48 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive datatypes
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('224')
+const anotherID = Symbol('224')
+
+console.log(id === anotherID);
+
+const bigNumber = 34859083498289859n
+
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["Batman", "Deadpool", "Ironman"]
+let myDetails = {
+    name : "Amir",
+    age : 24,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof anotherID);
